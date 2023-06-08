eede411c18b8e33aaf557d8bf5fe76a18161285a
Jiya Garg <100217150+Jiya-garg9971@users.noreply.github.com>
2023-06-08 13:52:25 +0530

    Create Modular Exponentiation.cpp

---
+++ Day3/Modular Exponentiation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+int modularExponentiation(int x, int n, int m) {
+	// Write your code here.
+	long long ans=1;
+	long long X=(long long)x;
+	long long M=(long long)m;
+	while(n){
+		if(n%2){
+			ans=ans*X;
+			n-=1;
+		}
+		else{
+			X=X*X;
+			X=(X+M)%M;
+			n/=2;
+		}
+		ans=(ans+M)%M;
+	}
+	return ans;
+}
