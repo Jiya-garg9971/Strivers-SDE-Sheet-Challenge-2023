fb0ad0099809f45c2e4c9c40924153574a7dd29c
Jiya Garg <100217150+Jiya-garg9971@users.noreply.github.com>
2023-06-08 11:35:45 +0530

    Create Majority Element - II.cpp
    https://leetcode.com/problems/majority-element-ii/

---
+++ Day3/Majority Element - II.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+vector<int> majorityElementII(vector<int> &arr)
+{
+    // Write your code here.
+    map<int,int> mp;
+    int n=arr.size();
+	for(int i=0;i<n;i++)mp[arr[i]]++;
+	int mx=0;
+    vector<int> ans;
+	for(auto i:mp){
+		if(i.second>n/3)ans.push_back(i.first);
+	}
+	return ans;
+
+}
