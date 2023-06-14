11c08979f3a239b86f76b755c65677b426df5c47
Jiya Garg <100217150+Jiya-garg9971@users.noreply.github.com>
2023-06-14 17:33:38 +0530

    Update Delete Node In A Linked List.cpp

--- Day5/Delete Node In A Linked List.cpp
+++ Day5/Delete Node In A Linked List.cpp
@@ -1,18 +1,5 @@
-
-Node* removeKthNode(Node* head, int K)
-{
-    // Write your code here
-    Node* slow=head;
-    int n=0;
-    while(slow!=NULL){
-        n++;
-        slow=slow->next;
-    }
-    Node* fast=head;
-    for(int i=0;i<n-K-1;i++){
-        fast=fast->next;
-    }
-    if(K==n)return head->next;
-    fast->next=fast->next->next;
-    return head;
+void deleteNode(LinkedListNode<int> * node) {
+    // Write your code here.
+    swap(node->data,node->next->data);
+    node->next=node->next->next;
 }
