1675e0284e99ae4f21cc2e2e132ad6d84902b06b
Jiya Garg <100217150+Jiya-garg9971@users.noreply.github.com>
2023-06-14 17:32:47 +0530

    Create Delete Kth Node From End.cpp

---
+++ Day5/Delete Kth Node From End.cpp
@@ -0,0 +1,17 @@
+Node* removeKthNode(Node* head, int K)
+{
+    // Write your code here
+    Node* slow=head;
+    int n=0;
+    while(slow!=NULL){
+        n++;
+        slow=slow->next;
+    }
+    Node* fast=head;
+    for(int i=0;i<n-K-1;i++){
+        fast=fast->next;
+    }
+    if(K==n)return head->next;
+    fast->next=fast->next->next;
+    return head;
+}
